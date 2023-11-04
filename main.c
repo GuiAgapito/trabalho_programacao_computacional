c68e4465ad9b0efd6b807cbe0f27a97a1a47ea78
Guilherme Agapito <103326257+GuiAgapito@users.noreply.github.com>
2023-11-04 12:07:59 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,56 +1,95 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
-int main (){
+int teste01() {
 
-    int quant_eq;
-    printf ("Digite a quantidade de equipes: ");
-    scanf ("%d", &quant_eq);
-    
-    int num_eq[quant_eq], posicao[quant_eq];
-    for (int i=1; i<=quant_eq; i++){
-    printf ("\nDigite a numeração da equipe %d:", i);
-    scanf ("%d", &num_eq[i]);
-    }
-    
-    // for (int j=1; j<=quant_eq; j++){
-    // posicao[j] = (1 + rand() % quant_eq);
-    // printf ("%d", posicao[j]);
-    // //printf("Equipe %d: %d ", j, rand() % (quant_eq + 1));
-    // }
+    int quantidade_equipes;
 
-    int numeros_sorteados[quant_eq];
-    int min = 1;
-    int max = quant_eq;
+    printf("Digite a quantidade de equipes que você deseja sortear: ");
+    scanf("%d", &quantidade_equipes);
 
-    // Inicialize o gerador de números aleatórios com uma semente diferente
-    srand(time(NULL));
+    int numeros_equipes[quantidade_equipes];
 
-    for (int i = 0; i < max; i++) {
-        int numero_aleatorio;
-        do {
-            numero_aleatorio = rand() % (max - min + 1) + min; // Gera um número entre 1 e 7
-            // Verifica se o número já foi sorteado antes
-            int repetido = 0;
-            for (int j = 0; j < i; j++) {
-                if (numero_aleatorio == numeros_sorteados[j]) {
-                    repetido = 1;
-                    break;
-                }
-            }
-            if (!repetido) {
-                numeros_sorteados[i] = numero_aleatorio;
-                break;
-            }
-        } while (1);
-    }
-
-    printf("Números sorteados: ");
-    for (int i = 0; i < quant_eq; i++) {
-        printf("%d ", numeros_sorteados[i]);
-    }
     printf("\n");
+
+    for (int i = 1; i <= quantidade_equipes; i++) {
+        printf("Digite a numeração da %dª equipe: ", i);
+        scanf("%d", &numeros_equipes[i]);
+    }
+
+    int matriz[2][quantidade_equipes];
+
+    printf("\n");
+
+    for (int i = 1; i <= 2; i++) {
+        for (int j = 1; j <= quantidade_equipes; j++) {
+            if (i == 1 && j == 1) {
+                printf("Equipes: ");
+            }
+            if (i == 2  && j == 1) {
+                printf("Ordem:   ");
+            }
+            if (i == 1) {
+                matriz[1][j] = numeros_equipes[j];                
+                printf("%d  ", matriz[i][j]);
+            } else {
+                printf("%d  ", j);
+            }
+        }
+        printf("\n");        
+    }
+
+    printf("\n");    
+
+    return 0;
+}
+
+int teste02() {
+
+    int quantidade_equipes;
+
+    printf("Digite a quantidade de equipes que você deseja sortear: ");
+    scanf("%d", &quantidade_equipes);
+
+    int numeros_equipes[quantidade_equipes];
+
+    printf("\n");
+
+    for (int i = 1; i <= quantidade_equipes; i++) {
+        printf("Digite a numeração da %dª equipe: ", i);
+        scanf("%d", &numeros_equipes[i]);
+    }
+
+    int matriz[2][quantidade_equipes];
+
+    printf("\nOrdem inicial para o sorteio: \n\n");
+
+    for (int i = 1; i <= 2; i++) {
+        for (int j = 1; j <= quantidade_equipes; j++) {
+            if (i == 1 && j == 1) {
+                printf("Equipes: ");
+            }
+            if (i == 2  && j == 1) {
+                printf("Ordem:   ");
+            }
+            if (i == 1) {
+                matriz[1][j] = numeros_equipes[j];                
+                printf("%d  ", matriz[i][j]);
+            } else {
+                printf("%d  ", j);
+            }
+        }
+        printf("\n");        
+    }
+
+    printf("\n");    
+
+    return 0;   
+
+}
+
+int main() {
+    
+    teste02();
 
     return 0;
 }
