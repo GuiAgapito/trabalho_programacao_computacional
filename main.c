fc7650769894a4755899e32da42bcba9787e2945
GuiAgapito <guilherme.agapito2004@gmail.com>
2023-11-06 12:54:39 -0300

    Hi

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 
+// Função para ordenar
 int ordenacao(int array[], int N) {
     int temp;
     for (int i = 1; i <= N; i++) {
@@ -39,69 +40,63 @@ int main()
             scanf("%d%*c", &matriz[0][i]);
         }       
 
-        // Sorteio sem repetição
-        for (int i = 0; i < quantidade_equipes; i++) {
-            int randIndex = i + rand() % (quantidade_equipes - i);
-            int temp = matriz[0][i];
-            matriz[0][i] = matriz[0][randIndex];
-            matriz[0][randIndex] = temp;
-        }
+        // // Sorteio sem repetição
+        // for (int i = 0; i < quantidade_equipes; i++) {
+        //     int randIndex = i + rand() % (quantidade_equipes - i);
+        //     int temp = matriz[0][i];
+        //     matriz[0][i] = matriz[0][randIndex];
+        //     matriz[0][randIndex] = temp;
+        // }
 
-        for (int i = 1; i <= quantidade_equipes; i++)
-        {
-            matriz[1][i] = i + 1;
-        } 
+        // for (int i = 1; i <= quantidade_equipes; i++)
+        // {
+        //     matriz[1][i] = i + 1;
+        // } 
 
-        ordenacao(matriz[1], quantidade_equipes);
+        // ordenacao(matriz[1], quantidade_equipes);
 
-        // Exibe a matriz ordenada
-        printf("\nEquipe\tOrdem de Apresentacao\n");
-        for (int i = 0; i < quantidade_equipes; i++) {
-            printf("%d\t%d\n", matriz[0][i], matriz[1][i]);
-        }
+        // // // Matriz que irá mostrar a ordem e a numeração das equipes
+        // // int matriz[2][quantidade_equipes];
 
-        // // Matriz que irá mostrar a ordem e a numeração das equipes
-        // int matriz[2][quantidade_equipes];
-
-        // // Mostra a ordem inicial antes do sorteio
-        // printf("\nNumeração e ordem das equipes antes do sorteio: \n");
-        // for (int linha = 1; linha <= 2; linha++) {
-        //     for (int coluna = 1; coluna <= quantidade_equipes; coluna++) {
-        //         if (linha == 1) {
-        //             matriz[1][coluna] = equipes[coluna];
-        //             printf("%d  ", matriz[linha][coluna]);
-        //         }
-        //         else {
-        //             printf("%d  ", coluna);
-        //         }
-        //     }
-        //     printf("\n");
-        // }        
+        // // // Mostra a ordem inicial antes do sorteio
+        // // printf("\nNumeração e ordem das equipes antes do sorteio: \n");
+        // // for (int linha = 1; linha <= 2; linha++) {
+        // //     for (int coluna = 1; coluna <= quantidade_equipes; coluna++) {
+        // //         if (linha == 1) {
+        // //             matriz[1][coluna] = equipes[coluna];
+        // //             printf("%d  ", matriz[linha][coluna]);
+        // //         }
+        // //         else {
+        // //             printf("%d  ", coluna);
+        // //         }
+        // //     }
+        // //     printf("\n");
+        // // }        
 
         
-        // for (int i = 1; i <= quantidade_equipes; i++) {
-        //     for (int j = 1; j <= quantidade_equipes - 1 - i; j++) {
-        //         if (equipes[j] < equipes[j + 1]) {
-        //             int aux = equipes[j];
-        //             equipes[j] = equipes[j + 1];
-        //             equipes[j + 1] = aux;
-        //         }
-        //     }
-        // }
+        // // for (int i = 1; i <= quantidade_equipes; i++) {
+        // //     for (int j = 1; j <= quantidade_equipes - 1 - i; j++) {
+        // //         if (equipes[j] < equipes[j + 1]) {
+        // //             int aux = equipes[j];
+        // //             equipes[j] = equipes[j + 1];
+        // //             equipes[j + 1] = aux;
+        // //         }
+        // //     }
+        // // }
 
-        // printf("\nOrdem de apresentação após o sorteio: \n");
-        // for (int linha = 1; linha <= 2; linha++) {
-        //     for (int coluna = 1; coluna <= quantidade_equipes; coluna++) {
-        //         if (linha == 1) {
-        //             matriz[1][coluna] = equipes[coluna];
-        //             printf("%d  ", matriz[linha][coluna]);
-        //         }
-        //         else {
-        //             printf("%d  ", coluna);
-        //         }
-        //     }
-        //     printf("\n");
-        // }
+        // // printf("\nOrdem de apresentação após o sorteio: \n");
+        // // for (int linha = 1; linha <= 2; linha++) {
+        // //     for (int coluna = 1; coluna <= quantidade_equipes; coluna++) {
+        // //         if (linha == 1) {
+        // //             matriz[1][coluna] = equipes[coluna];
+        // //             printf("%d  ", matriz[linha][coluna]);
+        // //         }
+        // //         else {
+        // //             printf("%d  ", coluna);
+        // //         }
+        // //     }
+        // //     printf("\n");
+        // // }
         
         printf("\nVocê deseja sortear novamente (S para SIM / N para NÃO )? ");
         scanf(" %c", &pergunta);
